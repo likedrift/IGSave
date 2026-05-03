1a606cbb0aa2bd9a1763b21197f1953841d90f7d
yank1 <809038249@qq.com>
2026-05-04 03:34:03 +0800

    Initial Commit

---
+++ ig_save/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ig_save
+//
+//  Created by yank on 2026/5/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ig_save/ig_saveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ig_saveApp.swift
+//  ig_save
+//
+//  Created by yank on 2026/5/4.
+//
+
+import SwiftUI
+
+@main
+struct ig_saveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
